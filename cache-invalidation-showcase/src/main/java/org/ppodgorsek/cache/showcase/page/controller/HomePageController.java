a5dfff8ec87c04b39dfdbfc29c754f87e9570975
Paul Podgorsek <ppodgorsek@users.noreply.github.com>
2014-11-12 09:47:43 +0100

    Created the Cache Invalidation Showcase module.

---
+++ cache-invalidation-showcase/src/main/java/org/ppodgorsek/cache/showcase/page/controller/HomePageController.java
@@ -0,0 +1,15 @@
+package org.ppodgorsek.cache.showcase.page.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controller for the home page.
+ * 
+ * @author Paul Podgorsek
+ */
+@Controller
+@RequestMapping("/home.page")
+public class HomePageController {
+
+}
