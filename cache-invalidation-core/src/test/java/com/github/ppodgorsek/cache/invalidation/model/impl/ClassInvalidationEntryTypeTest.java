a4297688fabb4ceef44fa53ec81762364027275f
Paul Podgorsek <ppodgorsek@users.noreply.github.com>
2016-01-09 02:39:48 +0000

    Added new tests.

--- cache-invalidation-core/src/test/java/com/github/ppodgorsek/cache/invalidation/model/impl/ClassInvalidationEntryTypeTest.java
+++ cache-invalidation-core/src/test/java/com/github/ppodgorsek/cache/invalidation/model/impl/ClassInvalidationEntryTypeTest.java
@@ -1,7 +1,9 @@
 package com.github.ppodgorsek.cache.invalidation.model.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -21,12 +23,39 @@ public class ClassInvalidationEntryTypeTest {
 	}
 
 	@Test
+	public void equalsWithSameInstance() {
+		assertTrue("The equals method should return true",
+				classInvalidationEntryType.equals(classInvalidationEntryType));
+	}
+
+	@Test
+	public void equalsWithDifferentInstanceHavingSameClass() {
+		assertTrue("The equals method should return true",
+				classInvalidationEntryType.equals(new ClassInvalidationEntryType(Object.class)));
+	}
+
+	@Test
+	public void equalsWithDifferentInstanceHavingDifferentClass() {
+		assertFalse("The equals method should return false",
+				classInvalidationEntryType.equals(new ClassInvalidationEntryType(
+						ClassInvalidationEntryType.class)));
+	}
+
+	@Test
 	public void getValueWithCorrectClass() {
 
 		final String value = classInvalidationEntryType.getValue();
 
 		assertNotNull("The value shouldn't be null", value);
 		assertEquals("Wrong value", Object.class.getSimpleName(), value);
+	}
+
+	@Test
+	public void hashCodeWithCorrectValue() {
+
+		final int hashCode = classInvalidationEntryType.hashCode();
+
+		assertTrue("Wrong hashCode", hashCode != 0);
 	}
 
 	@Test
