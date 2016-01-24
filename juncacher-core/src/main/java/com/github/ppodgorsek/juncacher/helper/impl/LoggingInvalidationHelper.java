d7993a7e211476b886a8ad58ca249ec188c33c61
Paul Podgorsek <ppodgorsek@users.noreply.github.com>
2016-01-24 15:38:11 +0000

    Fixed compilation errors.

--- juncacher-core/src/main/java/com/github/ppodgorsek/juncacher/helper/impl/LoggingInvalidationHelper.java
+++ juncacher-core/src/main/java/com/github/ppodgorsek/juncacher/helper/impl/LoggingInvalidationHelper.java
@@ -7,6 +7,7 @@ import com.github.ppodgorsek.juncacher.exception.InvalidationException;
 import com.github.ppodgorsek.juncacher.helper.AbstractChainedInvalidationHelper;
 import com.github.ppodgorsek.juncacher.helper.InvalidationHelper;
 import com.github.ppodgorsek.juncacher.model.InvalidationEntry;
+import com.github.ppodgorsek.juncacher.strategy.InvalidationStrategy;
 
 /**
  * {@link InvalidationHelper} implementation that simply logs invalidation entries.
@@ -15,12 +16,14 @@ import com.github.ppodgorsek.juncacher.model.InvalidationEntry;
  * @author Paul Podgorsek
  */
 public class LoggingInvalidationHelper<T extends InvalidationEntry>
-		extends AbstractChainedInvalidationHelper<T> implements InvalidationHelper<T> {
+		extends AbstractChainedInvalidationHelper<T, InvalidationStrategy>
+		implements InvalidationHelper<T> {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingInvalidationHelper.class);
 
 	@Override
-	protected void invalidateEntry(final T entry) throws InvalidationException {
+	protected void invalidateEntry(final T entry, final InvalidationStrategy strategy)
+			throws InvalidationException {
 		LOGGER.info("Invalidating an entry: {}", entry);
 	}
 
